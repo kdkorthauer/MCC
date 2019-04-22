d5b4eafc0e4b294ba33489a8a5bf90917733de40
kdkorthauer <kdkorthauer@gmail.com>
2019-04-22 14:27:17 -0400

    Download mCG dropbox; add genes of interest

--- ANALYSIS/mCG/download/20190409_downloaddropbox.sh
+++ ANALYSIS/mCG/download/20190409_downloaddropbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 1                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
-#SBATCH -t 1-00:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-10:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p commons,shared,serial_requeue   # Partition to submit to
 #SBATCH --mem=20G           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o ../_slurm/downloaddropbox_%j.out  # File to which STDOUT will be written, %j inserts jobid
@@ -10,10 +10,5 @@
 module load rclone
 cd ../../../DATA/mCG/wgbs
 
-rclone copy "dropbox:MCC/Bismark Bisulfite Data - Ben Singer" .
-
-cd Singer_Bisulfite_Bismark_cov
-tar -xvf Singer_WBGS_cov.tar.gz 
-
-cd ../Singer_Bisulfite_Bismark_txt
-tar -xvf Singer_WBGS_txt.tar.gz 
+rclone copy "dropbox:MCC/Bismark Bisulfite Data - Ben Singer - wgbs" .
+# wgbs in cov dir
