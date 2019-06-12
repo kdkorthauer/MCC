c81ab09af567c575d06ef4f48c1de92fcca0cee8
kdkorthauer <kdkorthauer@gmail.com>
2019-06-12 09:59:48 -0400

    Add startup command for globus

--- ANALYSIS/NIH/20190605_downoadGlobusData.sh
+++ ANALYSIS/NIH/20190605_downoadGlobusData.sh
@@ -7,4 +7,6 @@
 #SBATCH -o scratch/slurm/mv_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e scratch/slurm/mv_%j.err  # File to which STDERR will be written, %j inserts jobid
 
+/n/irizarryfs01_backed_up/kkorthauer/softwareTools/globusconnectpersonal-2.3.6/globusconnectpersonal -start &
+
 globus transfer --recursive f6657116-4a1f-11e8-8f88-0a6d4e044368:for_keegan/ 766d20f2-87b4-11e9-8e6a-029d279f7e24:/n/irizarryfs01/kkorthauer/MCC/DATA/NIH/
