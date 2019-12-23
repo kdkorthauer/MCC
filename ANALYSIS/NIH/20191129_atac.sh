fcb4830ac719e2f1dd11d162a769c0f3172851bb
kdkorthauer <kdkorthauer@gmail.com>
2019-12-23 14:54:58 -0800

    update atac slurm submit script

--- ANALYSIS/NIH/20191129_atac.sh
+++ ANALYSIS/NIH/20191129_atac.sh
@@ -1,17 +1,27 @@
+#!/bin/bash
+#SBATCH -J ATAC
+#SBATCH -n 10
+#SBATCH -N 1
+#SBATCH --mem 20G
+#SBATCH -t 0-5:00
+#SBATCH -o slurm/MCCrna-%j.out
+#SBATCH -e slurm/MCCrna-%j.err
+
 
 cd /rafalab/keegan/MCC/DATA/NIH/atac_out/signal/macs2/rep1/
 
-mkdir /rafalab/keegan/MCC/DATA/NIH/atac_out/signal/macs2/rep1/bedgraph
+mkdir -p /rafalab/keegan/MCC/DATA/NIH/atac_out/signal/macs2/rep1/bedgraph
 
 for files in ./*.bigwig
 do
   out=${files/.bigwig/.bedGraph}
   out=${out/.\//.\/bedgraph\/}
-  echo $out
-  bigWigToBedGraph $files $out
+  if [ ! -f "$out" ]; then
+    echo $out
+    bigWigToBedGraph $files $out
+  fi
 done
 
-
-
+R CMD BATCH --quiet --no-restore --no-save /rafalab/keegan/MCC/ANALYSIS/NIH/20191129_atac.R /rafalab/keegan/MCC/ANALYSIS/NIH/20191129_atac.Rout
 
 
